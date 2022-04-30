b13b87cf3e9035997b598b5e6160bff3fadc63a8
Egor-N-S <63055406+Egor-S-N@users.noreply.github.com>
2022-04-30 19:12:40 +0300

    Delete TextEditor directory

--- TextEditor/main.py
+++
@@ -1,9 +0,0 @@
-import sys, Form
-
-from PyQt5 import QtWidgets
-
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    window = Form.MainForm()
-    sys.exit(app.exec_())
-     
